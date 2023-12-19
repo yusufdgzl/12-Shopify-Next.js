549ec8011654d5f0cecb8c27938ee3bee3cdbe37
yusufdgzl <yusuf.dg13@gmail.com>
2023-12-19 05:10:07 +0300

    dynamic page creation and product-detail page

--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -1,11 +1,34 @@
-import ProductDetailCard from "@/components/products/product-detail-card"
+import ProductDetailCard from "@/components/products/product-detail-card";
+import { useParams } from "next/navigation";
+import { useEffect } from "react";
+import { useState } from "react";
 
-function ProductDetailPage(){
-    return (
-        <div>
-            <ProductDetailCard/>
-        </div>
-    )
+function ProductDetailPage() {
+  const [product, setProduct] = useState({});
+  const params = useParams();
+
+  useEffect(() => {
+    if (params) {
+      fetch("/products.json")
+        .then((res) => res.json())
+        .then((data) => {
+          const selectedProduct = data.find(
+            (item) => item.id === parseInt(params.productId)
+          );
+          setProduct(selectedProduct);
+        });
+    }
+  }, [params]);
+
+  if(!params){
+    return // Mission : Add Loading Animation
+  }
+
+  return (
+    <div>
+      <ProductDetailCard {...product} />
+    </div>
+  );
 }
 
-export default ProductDetailPage
+export default ProductDetailPage;
